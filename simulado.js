beda1e1d644f07e0d7d38cacb07b8ac160c12b0e
Francisco Hoffmann <76414601+afkhoffmann@users.noreply.github.com>
2023-06-20 02:48:25 -0300

    ww

--- simulado.js
+++ simulado.js
@@ -1,122 +1,84 @@
-let temas = [];
-let perguntas = [];
+function exibirPerguntas(temas, perguntas) {
+    const conteudo = document.getElementById('conteudo');
+    conteudo.innerHTML = '';
 
-fetch('temas.csv')
-    .then(response => response.text())
-    .then(data => {
-        let linhas = data.split('\n');
-        linhas.forEach(linha => {
-            let campos = linha.split(';');
-            temas.push({
-                tema: campos[0],
-                descricao: campos[1],
-                resumo: campos[2],
-                complemento: campos[3]
-            });
-        });
-
-        fetch('perguntas.csv')
-            .then(response => response.text())
-            .then(data => {
-                let linhas = data.split('\n');
-                linhas.forEach(linha => {
-                    let campos = linha.split(';');
-                    perguntas.push({
-                        tema: campos[0],
-                        pergunta: campos[1],
-                        resposta: campos[2],
-                        correcao: campos[3]
-                    });
-                });
-
-                montarProva();
-            });
-    });
-
-function montarProva() {
-    let conteudo = document.getElementById('conteudo');
-    let i = 1;
     temas.forEach(tema => {
-        let divTema = document.createElement('div');
-        let h2 = document.createElement('h2');
-        h2.innerText = tema.descricao;
+        const divTema = document.createElement('div');
+        const h2 = document.createElement('h2');
+        h2.textContent = tema.descricao;
+
+        const pResumo = document.createElement('p');
+        pResumo.textContent = tema.resumo;
+
         divTema.appendChild(h2);
-
-        let pResumo = document.createElement('p');
-        pResumo.innerText = tema.resumo;
         divTema.appendChild(pResumo);
+        
+        const pComplemento = document.createElement('p');
+        divTema.appendChild(pComplemento);
 
-        perguntas.filter(pergunta => pergunta.tema == tema.tema)
-            .forEach(pergunta => {
-                let divPergunta = document.createElement('div');
-                let h3 = document.createElement('h3');
-                h3.innerText = 'Pergunta ' + i;
-                divPergunta.appendChild(h3);
-                let p = document.createElement('p');
-                p.innerText = pergunta.pergunta;
-                divPergunta.appendChild(p);
+        perguntas.filter(p => p.tema == tema.tema).forEach((pergunta, index) => {
+            const divPergunta = document.createElement('div');
+            const h3 = document.createElement('h3');
+            h3.textContent = 'Pergunta ' + (index + 1);
+            divPergunta.appendChild(h3);
 
-                let label1 = document.createElement('label');
-                label1.innerText = 'Certo';
-                let radio1 = document.createElement('input');
-                radio1.type = 'radio';
-                radio1.name = 'pergunta' + i;
-                radio1.value = '1';
-                label1.prepend(radio1);
-                divPergunta.appendChild(label1);
+            const pPergunta = document.createElement('p');
+            pPergunta.textContent = pergunta.pergunta;
+            divPergunta.appendChild(pPergunta);
 
-                let label2 = document.createElement('label');
-                label2.innerText = 'Errado';
-                let radio2 = document.createElement('input');
-                radio2.type = 'radio';
-                radio2.name = 'pergunta' + i;
-                radio2.value = '0';
-                label2.prepend(radio2);
-                divPergunta.appendChild(label2);
+            ['Certo', 'Errado'].forEach((opcao, i) => {
+                const input = document.createElement('input');
+                input.type = 'radio';
+                input.name = 'pergunta' + pergunta.pergunta;
+                input.value = i;
+                divPergunta.appendChild(input);
 
-                divPergunta.dataset.resposta = pergunta.resposta;
-                divPergunta.dataset.correcao = pergunta.correcao;
-                divPergunta.dataset.radioCerto = radio1.id = 'pergunta' + i + 'opcao1';
-                divPergunta.dataset.radioErrado = radio2.id = 'pergunta' + i + 'opcao2';
-                divTema.appendChild(divPergunta);
-
-                i++;
+                const label = document.createElement('label');
+                label.textContent = opcao;
+                divPergunta.appendChild(label);
             });
+
+            divTema.appendChild(divPergunta);
+        });
 
         conteudo.appendChild(divTema);
     });
 }
 
 function corrigir() {
-    let divsPergunta = document.querySelectorAll('#conteudo > div > div');
-    let acertos = 0;
-    let total = divsPergunta.length;
+    const temas = carregarCSV('temas.csv', ['tema', 'descricao', 'resumo', 'complemento']);
+    const perguntas = carregarCSV('perguntas.csv', ['tema', 'pergunta', 'resposta', 'correcao']);
+    let corretas = 0;
 
-    divsPergunta.forEach(div => {
-        let resposta = div.dataset.resposta;
-        let selecionado = document.querySelector(`input[name='${div.querySelector('input').name}']:checked`);
-        let idRadioCorreto = resposta == '1' ? div.dataset.radioCerto : div.dataset.radioErrado;
-
-        if (selecionado) {
-            if (selecionado.value === resposta) {
-                acertos++;
-                document.getElementById(idRadioCorreto).parentNode.style.backgroundColor = 'lightgreen';
-            } else {
-                selecionado.parentNode.style.backgroundColor = 'pink';
+    temas.forEach((tema, tIndex) => {
+        perguntas.filter(p => p.tema == tema.tema).forEach((pergunta, pIndex) => {
+            const divPergunta = document.getElementById('conteudo').children[tIndex].children[pIndex + 2];
+            const inputs = divPergunta.getElementsByTagName('input');
+            const labels = divPergunta.getElementsByTagName('label');
+            for(let i = 0; i < inputs.length; i++) {
+                inputs[i].parentElement.classList.remove('correto', 'errado');
+                if(inputs[i].checked) {
+                    if(inputs[i].value == pergunta.resposta) {
+                        labels[i].textContent = labels[i].textContent.split(". ")[0] + ". " + perguntas.correcao;
+                        labels[i].parentElement.classList.add('correto');
+                        corretas++;
+                    } else {
+                        labels[i].textContent = labels[i].textContent.split(". ")[0] + ". " + perguntas.correcao;
+                        labels[i].parentElement.classList.add('errado');
+                    }
+                }
             }
-            document.getElementById(idRadioCorreto).innerText = resposta == '1' ? 'Certo. ' + div.dataset.correcao : 'Errado. ' + div.dataset.correcao;
-        }
+        });
+
+        const pComplemento = document.getElementById('conteudo').children[tIndex].children[2 + perguntas.filter(p => p.tema == tema.tema).length];
+        pComplemento.textContent = tema.complemento;
     });
 
-    document.getElementById('resultado').innerText = `Você acertou ${acertos} de ${total} (${(acertos / total * 100).toFixed(2)}%)`;
-
-    temas.forEach(tema => {
-        let divTema = Array.from(document.querySelectorAll('#conteudo > div')).find(div => div.querySelector('h2').innerText == tema.descricao);
-        if (!divTema.querySelector('.complemento')) {
-            let pComplemento = document.createElement('p');
-            pComplemento.className = 'complemento';
-            pComplemento.innerText = tema.complemento;
-            divTema.appendChild(pComplemento);
-        }
-    });
+    document.getElementById('resultado').textContent = `Você acertou ${corretas} de ${perguntas.length} (${(corretas/perguntas.length*100).toFixed(2)}%)`;
 }
+
+window.onload = () => {
+    const temas = carregarCSV('temas.csv', ['tema', 'descricao', 'resumo', 'complemento']);
+    const perguntas = carregarCSV('perguntas.csv', ['tema', 'pergunta', 'resposta', 'correcao']);
+    exibirPerguntas(temas, perguntas);
+};
